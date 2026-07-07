e46d429b30f9cd7690b3a419a7025ec5c869648e
heitorodrigue <heitorodrigues486@gmail.com>
2026-07-07 19:42:25 -0300

    feat: implementa gerenciamento de pessoas

---
+++ backend/LarFix.Api/Controllers/PessoaController.cs
@@ -0,0 +1,44 @@
+﻿using LarFix.Api.DTOs.Requests;
+using LarFix.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LarFix.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PessoaController : ControllerBase
+{
+    private readonly IPessoaService _pessoaService;
+
+    public PessoaController(IPessoaService pessoaService)
+    {
+        _pessoaService = pessoaService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Criar(PessoaCreateRequest request)
+    {
+        var pessoa = await _pessoaService.CriarAsync(request);
+
+        return StatusCode(StatusCodes.Status201Created, pessoa);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Listar()
+    {
+        var pessoas = await _pessoaService.ListarAsync();
+
+        return Ok(pessoas);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Excluir(int id)
+    {
+        var removido = await _pessoaService.ExcluirAsync(id);
+
+        if (!removido)
+            return NotFound();
+
+        return NoContent();
+    }
+}
